4fb8e07ea76549c12676f98a37bc17f211569958
Návan <navanmapp@gmail.com>
2024-01-18 15:47:54 +0200

    Middleware update

---
+++ server/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.header.authorization.split(' ')[1]
+        const decodeToken = jwt.verify(token, 'Random_Token_Secret')
+        const userId = decodeToken.userId
+
+        if(req.body.userId && req.body.user !== userId) {
+            throw 'Invalid user ID'
+        } else {
+            next()
+        }
+    } catch {
+        res.status(401).json({
+            error: new Error('Invalid request')
+        })
+    }
+}
--- server/routes/stuff.js
+++ server/routes/stuff.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const Thing = require('../models/Thing')
+
+const auth = require('../middleware/auth')
 
 const stuffCtrl = require('../controllers/stuff')
 
-router.get('/', stuffCtrl.getAllStuff)
-router.post('/', stuffCtrl.createThing)
-router.get('/:id', stuffCtrl.getOneThing)
-router.put('/:id', stuffCtrl.putThing)
-router.delete('/', stuffCtrl.deleteThing)
+router.get('/', auth, stuffCtrl.getAllStuff)
+router.post('/', auth, stuffCtrl.createThing)
+router.get('/:id', auth, stuffCtrl.getOneThing)
+router.put('/:id', auth, stuffCtrl.putThing)
+router.delete('/', auth, stuffCtrl.deleteThing)
 
 
 module.exports = router
